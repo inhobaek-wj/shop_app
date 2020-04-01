18d9e93b8196912ce3698afd146a613fb86749c2
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-01 19:35:30 +0900

    add screen ProductDetailScreen

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}

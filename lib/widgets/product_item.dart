278c2cc717475fca6167b2f93dcec0edaa14df3e
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-01 19:36:36 +0900

    working with provider & listener

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -14,35 +14,39 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
+    return GestureDetector(
+      onTap: () {},
 
-      footer: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
 
-        child: GridTileBar(
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-          ),
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: const Icon(Icons.favorite),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
-          ),
+        footer: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
 
-          trailing: IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
+          child: GridTileBar(
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: const Icon(Icons.favorite),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+
+            trailing: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
           ),
         ),
-      ),
 
+      ),
     );
   }
 

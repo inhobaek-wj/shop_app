f12e84a1223aed2a8c81164e217318da2109c32b
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-01 20:08:30 +0900

    routing to ProductDetailScreen

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
 
   final String id;
@@ -15,7 +17,12 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        Navigator.of(context).pushNamed(
+          ProductDetailScreen.routeName,
+          arguments: id,
+        );
+      },
 
       child: GridTile(
         child: Image.network(

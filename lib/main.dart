0abf5b3d8246056afe41eb267ef7ea41a48b914c
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-02 21:11:09 +0900

    routing to OrdersScreen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'screens/products_overview_screen.dart';
 import 'screens/product_detail_screen.dart';
 import 'screens/cart_screen.dart';
+import 'screens/orders_screen.dart';
 import 'store/products.dart';
 import 'store/cart.dart';
 import 'store/order.dart';
@@ -53,6 +54,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName:  (ctx) => CartScreen(),
+          OrdersScreen.routeName:  (ctx) => OrdersScreen(),
         },
       ),
     );
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,7 +6,7 @@ import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
 
-  static const routeName = '/cart';
+  static const routeName = '/orders';
 
   @override
   Widget build(BuildContext context) {

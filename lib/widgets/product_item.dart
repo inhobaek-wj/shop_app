a5537fb5b2c3273a377c78e60388a427831a9ec8
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-02 12:39:17 +0900

    change Product as provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../store/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
 
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  const ProductItem(
-    this.id,
-    this.title,
-    this.imageUrl
-  );
+  // const ProductItem(
+  //   this.id,
+  //   this.title,
+  //   this.imageUrl
+  // );
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+
     return GestureDetector(
       onTap: () {
         Navigator.of(context).pushNamed(
           ProductDetailScreen.routeName,
-          arguments: id,
+          arguments: product.id,
         );
       },
 
       child: GridTile(
         child: Image.network(
-          imageUrl,
+          product.imageUrl,
           fit: BoxFit.cover,
         ),
 
@@ -35,7 +39,7 @@ class ProductItem extends StatelessWidget {
 
           child: GridTileBar(
             title: Text(
-              title,
+              product.title,
               textAlign: TextAlign.center,
             ),
             backgroundColor: Colors.black87,

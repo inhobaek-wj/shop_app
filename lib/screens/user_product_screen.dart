9eb71b2020ca81b3077ab3c04f7ea432251e9668
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-03 14:31:05 +0900

    routing to EditProductScreen

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
+import '../screens/edit_product_screen.dart';
 
 class UserProductScreen extends StatelessWidget {
 
@@ -19,7 +20,9 @@ class UserProductScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           )
         ],
       ),

26886fe2bead080235cb14e24322fda2849a97c0
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-16 15:40:15 +0900

    using AnimatedBuilder instead of set listener directly like _heightAnimation.addListener(() => setState(() {}));

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -128,7 +128,7 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
         curve: Curves.linear,
     ));
 
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
 
     super.initState();
   }
@@ -241,14 +241,19 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
       ),
       elevation: 8.0,
 
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-        // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+
+        builder: (ctx, ch) => Container(
+          // height: _authMode == AuthMode.Signup ? 320 : 260,
+          height: _heightAnimation.value.height,
+          constraints:
+          // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+          BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch,
+        ),
 
         child: Form(
           key: _formKey,

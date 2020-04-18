d912d8addbb515d61859682b85f95e3a40e61836
inhobaek-wj <inhobaek.wj@gmail.com>
2020-04-18 09:37:21 +0900

    using Sliver

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,17 +16,18 @@ class ProductDetailScreen extends StatelessWidget {
     ).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
 
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
                 tag: loadedProduct.id,
                 child: Image.network(
                   loadedProduct.imageUrl,
@@ -34,31 +35,55 @@ class ProductDetailScreen extends StatelessWidget {
                 ),
               ),
             ),
+          ),
 
-            SizedBox(height: 10),
+          SliverList(
+            delegate: SliverChildListDelegate([
+                SizedBox(height: 10),
 
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(
-                color: Colors.grey
-              ),
-            ),
+                Text(
+                  '\$${loadedProduct.price}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
 
-            SizedBox(height: 10),
+                SizedBox(height: 10),
 
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10,
-              ),
-              child: Text(
-                loadedProduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
-              ),
-            ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  child: Text(
+                    loadedProduct.description,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ),
 
-          ],
-        ),
+                SizedBox(height: 800,),
+
+            ]),
+          ),
+        ],
+
+        // child: Column(
+        //   children: <Widget>[
+        //     Container(
+        //       height: 300,
+        //       width: double.infinity,
+        //       child: Hero(
+        //         tag: loadedProduct.id,
+        //         child: Image.network(
+        //           loadedProduct.imageUrl,
+        //           fit: BoxFit.cover,
+        //         ),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+
       ),
     );
   }
